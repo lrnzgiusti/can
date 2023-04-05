1bded46aa66b5c5fbb49e802ca6835b16276fa9a
lrnzgiusti <giusti@diag.uniroma1.it>
2023-04-05 20:44:55 +0100

    Improved doc

--- layers/signal_lift.py
+++ layers/signal_lift.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  4 20:00:00 2022
 
-@author: ince
-"""
+
 
 import torch
 import torch.nn as nn
@@ -102,80 +99,106 @@ class LiftLayer(nn.Module):
         return edge_signal
 
 
-
 class MultiHeadLiftLayer(nn.Module):
+    """
+    A multi-head lift layer for a Cellular Graph Attention Network (GAT).
+    This layer is responsible for lifting node feature vectors by 
+    exchanging information between edges weighted by learnable attention
+    coefficients, for multiple attention heads.
 
-    def __init__(self, F_in:int, K:int, 
+    Parameters
+    ----------
+    F_in : int
+        Number of input features for the lift layers.
+    K : int
+        Number of attention heads.
+    signal_lift_activation : Callable
+        Non-linear activation function for the signal lift.
+    signal_lift_dropout : float
+        Dropout rate applied after the lift.
+    signal_lift_readout : str
+        The readout strategy for combining the output of multiple attention heads.
+        Choices: 'cat', 'sum', 'avg', 'max'
+
+    Examples
+    --------
+    >>> multi_head_lift_layer = MultiHeadLiftLayer(F_in=10, K=3, signal_lift_activation=torch.relu, signal_lift_dropout=0.5, signal_lift_readout='sum')
+    """
+    def __init__(self, F_in: int, K: int,
                  signal_lift_activation: Callable,
                  signal_lift_dropout: float,
                  signal_lift_readout: str, *args, **kwargs):
-        """
-        
-
-        Parameters
-        ----------
-        F_in : int
-            Number of input features for the lift layers.
-        K : int
-            Numer of attention heads.
-        signal_lift_activation : Callable
-            non-linear activation function for the signal lift.
-        signal_lift_dropout : float
-            dropout is applied after the lift.
-
-        Returns
-        -------
-        Xe: torch.tensor
-            A signal defined over the edges of the graph
-        G: torch_geometric.dataset
-            the complex tha
-
-        """
         super(MultiHeadLiftLayer, self).__init__()
-        
+
         self.F_in = F_in
         self.K = K
         self.signal_lift_readout = signal_lift_readout
         self.signal_lift_dropout = signal_lift_dropout
         self.signal_lift_activation = signal_lift_activation
-        self.lifts = [LiftLayer(F_in=F_in, 
-                                signal_lift_activation=signal_lift_activation, 
+        self.lifts = [LiftLayer(F_in=F_in,
+                                signal_lift_activation=signal_lift_activation,
                                 signal_lift_dropout=signal_lift_dropout) for _ in range(K)]
-        
-        """
-        self.W = nn.Parameter(torch.empty(size=(K, 128)))
-        gain = nn.init.calculate_gain('relu')
-        nn.init.xavier_uniform_(self.W.data, gain=gain)
-        """
-        
 
     def __repr__(self):
-        str_F_out = '2' if self.signal_lift_readout != 'cat' else  str(2*self.K)
+        str_F_out = '2' if self.signal_lift_readout != 'cat' else str(2 * self.K)
         s = "MultiHeadLiftLayer(" + \
-            "F_in="+str(self.F_in)+", F_out=" + str_F_out + \
-            ", heads=" +str(self.K) + ", readout=" +self.signal_lift_readout+"):"
-            
+            "F_in=" + str(self.F_in) + ", F_out=" + str_F_out + \
+            ", heads=" + str(self.K) + ", readout=" + self.signal_lift_readout + "):"
+
         for idx, lift in enumerate(self.lifts):
-            s+= "\n\t(" +str(idx)+"): "+ str(lift)
-        return s 
-    
+            s += "\n\t(" + str(idx) + "): " + str(lift)
+        return s
+
     def to(self, device):
         self.lifts = nn.ModuleList([lift.to(device) for lift in self.lifts])
         return self
-        
+
     def forward(self, x):
-        x, G = x
-        Xe = [lift((x, G)) for lift in self.lifts] 
+        """
+        Perform the forward pass for a multi-head lift layer.
+
+        Parameters
+        ----------
+        x : Tuple[NodeSignal, Graph]
+            Input tuple containing the node signal (node feature vectors)
+            and the graph structure (Graph object).
+
+        Returns
+        -------
+        EdgeSignal
+            The resulting edge signal after the lift layer operation for multiple
+            attention heads.
+        Graph
+            The input graph structure.
+
+        Notes
+        -----
+        The forward pass can be described as follows:
+        1. Unpack the input tuple into node signal and graph structure.
+        2. Lift the node signal for each attention head.
+        3. Combine the output edge signals using the specified readout strategy.
+        4. Apply dropout to the combined edge signal.
+        """
+        # Unpack input tuple into node signal and graph structure
+        node_signal, graph = x
+
+        # Lift the node signal for each attention head
+        edge_signals = [lift((node_signal, graph)) for lift in self.lifts]
+
+        # Combine the output edge signals using the specified readout strategy
         if self.signal_lift_readout == 'cat':
-            Xe = torch.cat(Xe, dim=1) # Xe
+            combined_edge_signal = torch.cat(edge_signals, dim=1)
         elif self.signal_lift_readout == 'sum':
-            Xe = torch.stack(Xe, dim=2).sum(dim=2)
+            combined_edge_signal = torch.stack(edge_signals, dim=2).sum(dim=2)
         elif self.signal_lift_readout == 'avg':
-            Xe = torch.stack(Xe, dim=2).mean(dim=2)
+            combined_edge_signal = torch.stack(edge_signals, dim=2).mean(dim=2)
         elif self.signal_lift_readout == 'max':
-            Xe = torch.stack(Xe, dim=2).max(dim=2).values
-            
-        
-        Xe = F.dropout(Xe, self.signal_lift_dropout, training=self.training) #
-        return Xe, G
+            combined_edge_signal = torch.stack(edge_signals, dim=2).max(dim=2).values
+        else:
+            raise ValueError("Invalid signal_lift_readout value. Choose from ['cat', 'sum', 'avg', 'max']")
+
+        # Apply dropout to the combined edge signal
+        combined_edge_signal = F.dropout(combined_edge_signal, self.signal_lift_dropout, training=self.training)
+
+        return combined_edge_signal, graph
 
